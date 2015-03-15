4c2a2ba891f961c5eaba2d5a52e5e4818ae45ec5
Stephen Clem <stevebclem@gmail.com>
2015-03-15 13:45:59 -0700

    post routing is a go

--- server.js
+++ server.js
@@ -35,13 +35,13 @@ router.get('/spark_temp', function(req, res){
   })
 })
 
-router.post('/spark_up'), function(req, res){
+router.post('/spark_up', function(req, res){
   res.json("{message: you called the up function but it's not implemented yet!}")
-}
+})
 
-router.post('/spark_down'), function(req, res){
+router.post('/spark_down', function(req, res){
   res.json("{message: you called the down function but it's not implemented yet!}")
-}
+})
 
 router.get('/intel_api', function(req, res){
   unirest.get('http://api.wunderground.com/api/'+process.env.INTEL_API_KEY+'/conditions/q/CA/San_Francisco.json').end(function(data){
